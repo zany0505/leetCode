32fa02b41fbe1a6abbca0aa1e340aed7f7dc9d6e
azizi <zany0909@163.com>
2021-08-24 17:51:25 +0800

    day

--- src/main/java/leetcode/editor/cn/StringCompression.java
+++ src/main/java/leetcode/editor/cn/StringCompression.java
@@ -56,18 +56,48 @@ package leetcode.editor.cn;
 // 
 // Related Topics 双指针 字符串 👍 232 👎 0
 
-public class StringCompression{
+public class StringCompression {
     public static void main(String[] args) {
         Solution solution = new StringCompression().new Solution();
-        
+        char[] nums = {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+        solution.compress(nums);
     }
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int compress(char[] chars) {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int compress(char[] chars) {
+            int n = chars.length;
+            int write = 0, left = 0;
+            for (int read = 0; read < n; read++) {
+                if (read == n - 1 || chars[read] != chars[read + 1]) {
+                    chars[write++] = chars[read];
+                    int num = read - left + 1;
+                    if (num > 1) {
+                        int anchor = write;
+                        while (num > 0) {
+                            chars[write++] = (char) (num % 10 + '0');
+                            num /= 10;
+                        }
+                        reverse(chars, anchor, write - 1);
+                    }
+                    left = read + 1;
+                }
+            }
+            return write;
+        }
+
+        public void reverse(char[] chars, int left, int right) {
+            while (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
 
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
